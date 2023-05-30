efd324b104ee78e0e7cfa0891f970bcef1fdd2c9
Timm Rasmussen <timm.rasmussen@gmail.com>
2023-05-30 15:05:17 +0200

    Moh's navigation assignment, due date: 3 days...

---
+++ src/Navigation/NavigationSystem.java
@@ -0,0 +1,13 @@
+package Navigation;
+
+import Interface.Navigation;
+import org.opencv.core.Point;
+
+public class NavigationSystem implements Navigation {
+
+
+    @Override
+    public void goTo(Point src, Point dst, double direction) {
+        //TODO Moh Implement this
+    }
+}
