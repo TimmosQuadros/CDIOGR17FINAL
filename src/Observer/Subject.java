1be9bdb3d13dacd67ff8f1cf53a020308d948214
Timm Rasmussen <timm.rasmussen@gmail.com>
2023-05-30 11:01:44 +0200

    source folder...

---
+++ src/Observer/Subject.java
@@ -0,0 +1,30 @@
+package Observer
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+	
+	private List<Observer> observerCollection = new ArrayList<>();
+	
+	public void registerObserver(Observer observer) {
+		this.observerCollection.add(observer);
+	}
+	
+	public void unregisterObserver(Observer observer){
+		this.observerCollection.remove(observer);
+	}
+	
+	public void notifyObservers(Object object) {
+		for (Observer observer : observerCollection) {
+			observer.notify(object);
+		}
+	}
+	
+	public void notifyObservers() {
+		for (Observer observer : observerCollection) {
+			observer.notify();
+		}
+	}
+
+}
