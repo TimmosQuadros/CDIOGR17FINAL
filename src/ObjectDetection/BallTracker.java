b967453e0e08e1e8122e5bd4545f4cdbabc8d400
mudi26 <s215772@student.dtu.dk>
2023-06-01 13:06:02 +0200

    BallTracker class updated

--- src/ObjectDetection/BallTracker.java
+++ src/ObjectDetection/BallTracker.java
@@ -57,3 +57,53 @@ public class BallTracker {
 }
 
  */
+package ObjectDetection;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+public class BallTracker {
+    private static int totalBalls = 0;
+    private static int capturedBalls = 0;
+    private static int remainingBalls = 0;
+
+    public static void main(String[] args) {
+        // Initialize OpenCV
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Initialize video capture from the camera
+        VideoCapture videoCapture = new VideoCapture(0);
+
+        // Check if video capture is successful
+        if (!videoCapture.isOpened()) {
+            System.out.println("Failed to open video capture device.");
+            return;
+        }
+
+        // Main loop
+        while (true) {
+            // Read the current frame from the video capture
+            Mat frame = new Mat();
+            videoCapture.read(frame);
+
+            // Perform ball detection and tracking here
+            // ...
+
+            // Update the number of remaining balls
+            remainingBalls = totalBalls - capturedBalls;
+
+            // Print the remaining balls count
+            System.out.println("Remaining Balls: " + remainingBalls);
+
+            // Break the loop if desired conditions are met
+            // ...
+
+            // Break the loop if the 'Esc' key is pressed
+            //if (SomeConditionToBreakTheLoop)
+                break;
+        }
+
+        // Release the video capture
+        videoCapture.release();
+    }
+}
