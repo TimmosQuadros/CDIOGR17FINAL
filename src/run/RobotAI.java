e7bf11f7000ec86e0e81a3711cb30a448dfb142b
IverNuclear <emil1093@outlook.com>
2023-06-01 16:45:40 +0200

    Updateted floorcorners calculation

--- src/run/RobotAI.java
+++ src/run/RobotAI.java
@@ -1,8 +1,28 @@
 package run;
 
+import Observer.FindAreaOfInterestSubject;
+import Singleton.VideoCaptureSingleton;
+import org.opencv.core.Core;
+import org.opencv.core.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RobotAI {
 
-    public static void main(String[] args) {
+    //load the opencv library into the JVM at runtime
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
 
+    public static void main(String[] args) {
+        List<Point> corners;
+        List<Point> goals;
+
+        VideoCaptureSingleton videoCaptureSingleton = VideoCaptureSingleton.getInstance();
+
+        FindAreaOfInterestSubject areaOfInterestSubject = new FindAreaOfInterestSubject();
+        corners = areaOfInterestSubject.getCorners(videoCaptureSingleton);
+        goals = areaOfInterestSubject.getGoalPos(videoCaptureSingleton);
     }
 }
