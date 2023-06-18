6dd9314c3a59569392ede6983dbc95182d01a9a2
EFGottsche <esben.gottsche@gmail.com>
2023-06-18 16:08:22 +0200

    Direction checks added to prevent the robot from tracking ass balls

--- src/run/RobotAI.java
+++ src/run/RobotAI.java
@@ -29,9 +29,11 @@ public class RobotAI {
     private AlignRobot alignRobot;
     private Incoder incoder = new Incoder();
     private Server server;
+    private boolean robotFacingTop;
+    private boolean robotFacingWest;
 
 
-    public RobotAI(Server server){
+    public RobotAI(Server server) {
         FindAreaOfInterestSubject areaOfInterestSubject = new FindAreaOfInterestSubject();
         //corners = areaOfInterestSubject.getCorners();
         goals = areaOfInterestSubject.getGoalPos();
@@ -46,46 +48,59 @@ public class RobotAI {
         Math1.LineCreation lineCreation = new Math1.LineCreation();
         BallTracker ballTracker = new BallTracker();
         boolean targetingBall = false;
-        while(true){
-        RobotPositionSubject robotPositionSubject = new RobotPositionSubject();
-        HoughCircleDetectorSubject houghCircleDetectorSubject = new HoughCircleDetectorSubject();
+        while (true) {
+            RobotPositionSubject robotPositionSubject = new RobotPositionSubject();
+            HoughCircleDetectorSubject houghCircleDetectorSubject = new HoughCircleDetectorSubject();
 
-        boolean isBlueCircle = true; // Set the desired circle color preference here
+            boolean isBlueCircle = true; // Set the desired circle color preference here
 
-        List<Point> ballPositions = houghCircleDetectorSubject.getBalls();
-
-
-        List<Point> robotPosBlue = robotPositionSubject.getPos(isBlueCircle);
-        List<Point> robotPosRed = robotPositionSubject.getPos(!isBlueCircle);
-
-        Point redCircleRobot = robotPosRed.get(0);
-        Point blueCircleRobot = robotPosBlue.get(0);
-
-        String balls = incoder.ballPositions(ballPositions);
-        String robotPos1 = incoder.starPos(blueCircleRobot);
-
-            double[] line = lineCreation.getSlopeAndBegin(blueCircleRobot,redCircleRobot);
+            List<Point> ballPositions = houghCircleDetectorSubject.getBalls();
 
 
-            if(line!=null){
-                Point targetBall = ballTracker.checkIfBallIsOnLine(line,ballPositions,10.0);
-                if(targetBall!=null && !targetingBall){
+            List<Point> robotPosBlue = robotPositionSubject.getPos(isBlueCircle);
+            List<Point> robotPosRed = robotPositionSubject.getPos(!isBlueCircle);
+
+            Point redCircleRobot = robotPosRed.get(0);
+            Point blueCircleRobot = robotPosBlue.get(0);
+
+            robotFacingTop = facingDirection(redCircleRobot.y, blueCircleRobot.y);
+            robotFacingWest = facingDirection(redCircleRobot.x, blueCircleRobot.x);
+
+            String balls = incoder.ballPositions(ballPositions);
+            String robotPos1 = incoder.starPos(blueCircleRobot);
+
+            double[] line = lineCreation.getSlopeAndBegin(blueCircleRobot, redCircleRobot);
+
+
+            if (line != null) {
+                Point targetBall = ballTracker.checkIfBallIsOnLine(line, ballPositions, 10.0);
+                if (targetBall != null && !targetingBall) {
                     server.writeMessage(incoder.lineAB(line));
                     server.writeMessage(incoder.targetBall(targetBall));
                     System.out.println("ballFound");
                     targetingBall = true;
                 }
             }
-            if(targetingBall){
-                if(robotPosBlue.size()>0 && robotPosRed.size()>0){
+            if (targetingBall) {
+                if (robotPosBlue.size() > 0 && robotPosRed.size() > 0) {
                     server.writeMessage(incoder.starPos(blueCircleRobot));
                 }
             }
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+
+    /**
+     * @param firstCoordinate the coordinate for the front facing circle
+     * @param secondCoordinate the coordinate for the back facing circle
+     * @return the true or false if the robot is facing towards the direction in question
+     */
+    private boolean facingDirection(int firstCoordinate, int secondCoordinate){
+        return firstCoordinate-secondCoordinate < 0;
     }
 }
